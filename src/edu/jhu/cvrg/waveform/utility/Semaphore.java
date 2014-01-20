bcfd7972d2acd3f1b36c758b6fc5f02e89065291
avilard4 <avilard4@jhu.edu>
2014-01-20 09:29:55 -0500

    Fixes on multi-files uploads

---
+++ src/edu/jhu/cvrg/waveform/utility/Semaphore.java
@@ -0,0 +1,40 @@
+package edu.jhu.cvrg.waveform.utility;
+
+public class Semaphore {
+	  private int signals = 0;
+	  private int bound   = 0;
+
+	  private static Semaphore createFolderSemaphore = null;
+	  private static Semaphore createFileSemaphore = null;
+	  
+	  public static Semaphore getCreateFolderSemaphore(){
+		  if(createFolderSemaphore == null){
+			  createFolderSemaphore = new Semaphore(1);
+		  }
+		  return createFolderSemaphore;
+	  }
+	  
+	  public static Semaphore getCreateFileSemaphore(){
+		  if(createFileSemaphore == null){
+			  createFileSemaphore = new Semaphore(1);
+		  }
+		  return createFileSemaphore;
+	  }	  
+
+	  
+	  private Semaphore(int upperBound){
+	    this.bound = upperBound;
+	  }
+
+	  public synchronized void take() throws InterruptedException{
+	    while(this.signals == bound) wait();
+	    this.signals++;
+	    this.notify();
+	  }
+
+	  public synchronized void release() throws InterruptedException{
+	    while(this.signals == 0) wait();
+	    this.signals--;
+	    this.notify();
+	  }
+	}
