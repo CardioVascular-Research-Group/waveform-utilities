a32a4af03beda4fbb751129d0021192aa0acf2f2
avilard4 <avilard4@jhu.edu>
2014-02-10 15:05:07 -0500

    New Algorithms

--- src/edu/jhu/cvrg/waveform/model/PhysionetMethods.java
+++ src/edu/jhu/cvrg/waveform/model/PhysionetMethods.java
@@ -9,9 +9,13 @@ public enum PhysionetMethods{
 	SIGAAMP("sigamp", "sigampWrapperType2"),
 	SQRS("sqrs", "sqrsWrapperType2"),
 	SQRS2CSV("sqrs2csv", "sqrs2csvWrapperType2"),
+	SQRS4IHR("sqrs4ihr", "sqrs4ihrWrapperType2"),
+	SQRS4PNNLIST("sqrs4pnnlist/pNNx", "sqrs4pnnlistWrapperType2"),
 	TACH("tach", "tachWrapperType2"),
 	WQRS("wqrs", "wqrsWrapperType2"),
 	WQRS2CSV("wqrs2csv", "wqrs2csvWrapperType2"),
+	WQRS4IHR("wqrs4ihr", "wqrs4ihrWrapperType2"),
+	WQRS4PNNLIST("wqrs4pnnlist/pNNx", "wqrs4pnnlistWrapperType2"),
 	WRSAMP("wrsamp", "wrsampWrapperType2"), 
 	CHESNOKOV("QT Screening", "chesnokovWrapperType2");
 	
